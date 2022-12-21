5d4c4856a8422d01dd443062ecff2dbd97b07193
Amardeep6007 <amardeepkumar6007@gmail.com>
2022-12-21 10:30:58 +0530

    Updated the setup.py file again 3.0

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     name="sensor",
     version="0.0.1",
     author="ineuron",
-    author_email="avnish@ineuron.ai",
+    author_email="amardeepkumar6007@gmail.com",
     packages = find_packages(),
     install_requires=get_requirements(),
 )
