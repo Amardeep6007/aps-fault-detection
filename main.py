822b96fd51b51ca91c9e6be8a957a62c4f514f62
Amardeep6007 <amardeepkumar6007@gmail.com>
2022-12-22 18:56:51 +0530

    Updated the main.py file refer the comment section for the updated lines

--- main.py
+++ main.py
@@ -21,4 +21,25 @@ all_record = collection.find()
 
 # Printing all records present in the collection
 for idx, record in enumerate(all_record):
-     print(f"{idx}: {record}")
+    print(f"{idx}: {record}")
+
+
+#from sensor.logger import logging
+#from sensor.exception import SensorException
+#import sys, os
+
+#def test_loggingAndException():
+#     try:
+#          logging.info("starting the test_loggingAndException")
+#          res = 3/0
+#          print(res)
+#          logging.info("Stopping the test_loggingAndException")
+#     except Exception as e:
+#          logging.debug("Stopping the test_loggingAndException")
+#          raise SensorException(e, sys)
+
+#if __name__=="__main__":
+#     try:
+#          test_loggingAndException()
+#     except Exception as e:
+#          print(e)
---
+++ sensor/exception.py
@@ -0,0 +1,24 @@
+import sys,os
+
+#the below written function will provide the file name, line number and error name whenever we will be getting an error 
+def error_message_detail(error, error_detail: sys):
+    _, _, exc_tb = error_detail.exc_info()
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    error_message = "Error occurred python script name [{0}] line number [{1}] error message [{2}]".format(
+        file_name, exc_tb.tb_lineno, str(error)
+    )
+    return error_message
+
+
+
+class SensorException(Exception): #creating sensor exception class because we are working on then sesor data
+
+    def __init__(self,error_message, error_detail:sys):
+        self.error_message = error_message_detail(
+            error_message, error_detail=error_detail)
+
+    def __str__(self):
+        return self.error_message
+
+
+
