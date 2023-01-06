7cbe596f473d67349100155f983aec246e1eeead
Amardeep6007 <Amardeepkuamr6007@gmail.com>
2023-01-06 13:27:46 +0530

    update

--- sensor/components/model_trainer.py
+++ sensor/components/model_trainer.py
@@ -17,31 +17,66 @@ class ModelTrainer():
             pass
         except Exception as e:
             raise SensorException(e, sys)
+    
+    def fine_tune(self):
+
+        #write code for GridSearchCV
+        try:
+            pass
+        except Exception as e:
+            raise SensorException(e, sys)
 
     def train_model(self, x, y):
-        xgb_clf = XGBClassifier()
-        xgb_clf.fit(x,y)
-        return xgb_clf
+        try:
+            xgb_clf = XGBClassifier()
+            xgb_clf.fit(x,y)
+            return xgb_clf
+        except Exception as e:
+            raise SensorException(e, sys)
 
     def initiate_model_trainer(self, )->artifact_entity.ModelTrainerArtifact:
         try:
+            logging.info(f"Loading train and test arrray")
             train_arr = utils.load_numpy_array_data(file_path=self.data_transformation_artifact.transformed_train_path)
             test_arr = utils.load_numpy_array_data(file_path=self.data_transformation_artifact.transformed_test_path)
 
+            logging.info(f"splitting input and target feature from both train and test array")
             x_train, y_train = train_arr[:,:-1], train_arr[:,:-1]
             x_test, y_test = test_arr[:,:-1], test_arr[:,:-1]
 
+            logging.info(f"Trained the model")
             model = train_model(x=x_train, y=y_train)
+
+            logging.info(f"Calculating the f1 train score")
             yhat_train = model.predict(x_train)
             f1_train_score = f1_score(y_true = y_train, y_pred = yhat_train)
             
+            logging.info(f"Calculating the f1 test score")
             yhat_test = model.predict(x_test)
             f1_test_score = f1_score(y_true =y_test, y_pred = yhat_test)
 
+            logging.info(f"Train score:{f1_train_score} and test score:{f1_test_score} ")
             #check for overfitting and underfitting or expected score
+            logging.info(f"Checking if our model is underfitted model or not")
             if f1_test_score < self.model_trainer_config.expected_score:
                 raise Exception(f"Model is not a good model as it is not giving the expected accuracy {self.model_trainer_config.expected_score}:model actual score is: {f1_test_score}")
-                
+            
+            logging.info(f"Checking if our model is overfitted model or not")
+            diff = abs(f1_train_score-f1_test_score)
+            if(diff > self.model_trainer_config.overfitting_threshold):
+                raise Exception(f"Train and test score difference:{diff} \
+                    is more than overfitting threshold {self.model_trainer_config.overfitting_threshold}")
+            
+            #save the train model
+            logging.info(f"Saving the model object")
+            utils.save_object(file_path=self.model_trainer_config.model_path, obj= model)
 
+            #prepare artifact
+            logging.info(f"preparing the artifact")
+            model_trainer_artifact = artifact_entity.ModelTrainerArtifact(model_path=self.model_trainer_config, 
+                                                f1_train_score=f1_train_score, 
+                                                f1_test_score=f1_test_score)
+            logging.info(f"Model trainer artifact :{model_trainer_artifact}")
+            return model_trainer_artifact
         except Exception as e:
             raise SensorException(e, sys)           
